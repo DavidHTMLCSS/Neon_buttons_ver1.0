6b6caf2d9d3a472e791b11bb02d351a5f9f77f4f
DavidHTMLCSS <138385696+DavidHTMLCSS@users.noreply.github.com>
2023-07-09 22:37:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('butt1').addEventListener('click', function() {
+    document.getElementById('section1').scrollIntoView();
+});
+
+document.getElementById('butt2').addEventListener('click', function() {
+    document.getElementById('section2').scrollIntoView();
+});
+
+document.getElementById('butt3').addEventListener('click', function() {
+    document.getElementById('section3').scrollIntoView();
+});
+
+document.getElementById('butt4').addEventListener('click', function() {
+    document.getElementById('section4').scrollIntoView();
+});
